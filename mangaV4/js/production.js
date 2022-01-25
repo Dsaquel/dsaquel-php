4a44beb7fa35be4b1b76395bad7fe2cf85e11a44
Noblet Ouways <91417411+Dsaquel@users.noreply.github.com>
2022-01-25 10:48:13 +0100

    Delete mangaV4 directory

--- mangaV4/js/production.js
+++
@@ -1,9 +0,0 @@
-onload = function () {
-    const modal = document.getElementsByClassName("modal");
-    modal.style.display = "block";
-}
-
-document.querySelector('#mon-bouton').addEventListener('click', function(event) {
-    console.log(event.target);
-});
-
