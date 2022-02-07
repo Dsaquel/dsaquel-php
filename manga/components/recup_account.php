6bee49b6fa37bc672c24b3a0578cba8bd8741dcb
Noblet Ouways <91417411+Dsaquel@users.noreply.github.com>
2022-02-07 16:04:37 +0100

    Delete manga directory

--- manga/components/recup_account.php
+++
@@ -1,6 +0,0 @@
-<?php echo $_COOKIE['id'] ?>
-<form method="POST" action="config/recup_user_account.php">
-    <label>Voulez vous recuperez le compte ?</label>
-    <input type="hidden" name="id" value="<?php echo $_COOKIE['id']?>">
-    <button type="submit">OUI</button>
-</form>
