3cf7e5e628a650493ee43c6cd9fecde8e54aa866
Dsaquel <noblet.ouwaysgta5@gmail.com>
2022-02-11 16:49:09 +0100

    :monocle_face: dont update email and username if exist

--- src/config/update_user.php
+++ src/config/update_user.php
@@ -8,23 +8,35 @@ if (
     || !isset($postData['password'])
     || !isset($postData['username'])
 ) {
-    echo ('manque d info');
+    echo ('manque d\'info');
+} else {
+    $id = $postData['id_update'];
+    $email = $postData['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $username = $postData['username'];
+    
+    
+    $checkUsername = $mysqlClient->prepare('SELECT username FROM user WHERE username= :username');
+    $checkUsername->execute(array(
+        'username' => $username,
+    )) or die(print_r($checkUsername->errorInfo()));
+    
+    $num_rows_username = $checkUsername->rowCount();
+    if($num_rows_username == 0 || $_SESSION['username'] == $username) {
+        $updateUserQuery = $mysqlClient->prepare('UPDATE user SET email = :email, password = :password, username = :username where id = :id');
+        $updateUserQuery->execute([
+            'email' => $email,
+            'password' => $password,
+            'username' => $username,
+            'id' => $id,
+        ]);
+        
+        if ($updateUserQuery->execute()) {
+            header('Location: ../?userUpdate=true');
+        }
+    } else {
+        header('Location: ../?userUpdate=false');
+    }    
 }
 
-$id = $postData['id_update'];
-$email = $postData['email'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$username = $postData['username'];
-
-$updateUserQuery = $mysqlClient->prepare('UPDATE user SET email = :email, password = :password, username = :username where id = :id');
-$updateUserQuery->execute([
-    'email' => $email,
-    'password' => $password,
-    'username' => $username,
-    'id' => $id,
-]);
-
-if ($updateUserQuery->execute()) {
-    header('Location: ../?userUpdate=true');
-}
 ?>
